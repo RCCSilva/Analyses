d56dad07ddf91d2bd0e35c91cf3489bf3f33bf59
RCCSilva <rafa_castrosilva@hotmail.com>
2018-02-08 22:04:03 -0200

    tentativa de webscraping dos dados trimestrais da ssp-sp. Falta arrumar algumas coisas

--- [Trabalho]SSP/baixadados.R
+++ [Trabalho]SSP/baixadados.R
@@ -77,7 +77,91 @@ for(ano in 2001:2017){
 
 
 # atribuindo nome aos municipios ------------------------------------------
+rm(list = ls())
+
+source("[Trabalho]SSP/caso_especial.R")
+limpa <- function(tabela, ano){
+  
+  for(i in 1:nrow(tabela)){
+    if(tabela[i,1] == "ITEM"){
+      tabela <- tabela[i:nrow(tabela),]
+      break()
+    }
+  }
+  
+  nomes <- tabela[1,] %>% as.character()
+  
+  nomes[is.na(nomes) | nomes %in% c('',"NA")] <- str_c("excluir", 1:sum(is.na(nomes) | nomes %in% c("", "NA")))
+  
+  nomes <- str_to_lower(nomes)
+  colnames(tabela) <- c("ITEM", "ocorrencia", nomes[3:length(nomes)])
+  
+  tabela <- tabela %>% 
+    select(-starts_with("excluir")) %>% 
+    filter(!(str_detect(tabela$ITEM, "FONTE") | str_detect(tabela$ITEM, "\\(.*\\)")))
+}
+
+atr_item <- function(tabela){
+  tabela <- tabela %>% 
+    mutate(var = case_when(ITEM == "ITEM" ~ 1,
+                           T  ~ 0),
+           var = cumsum(var)) 
+  
+  itens <- tabela %>%
+    filter(ITEM == "ITEM") %>% 
+    rename("tipo" = "ocorrencia") %>% 
+    select(tipo, var)
+  
+  tabela %>% 
+    left_join(itens, by = "var")
+}
 
 
+caso_normal <- function(ano, tri){
+  url_base <- "http://www.ssp.sp.gov.br/Estatistica/plantrim/ANOTRI.htm"
+  
+  divisor <- ifelse(ano < 2004, "_", "-")
+  
+  url_uso <- str_replace(url_base, "ANOTRI", str_c(ano, divisor, "0",tri))
+  
+  tabela <- read_html(url_uso) %>% 
+    html_table(fill = T)
+  
+  tabela <- bind_rows(tabela)
+  
+  tabela <- limpa(tabela, ano)
+  
+  tabela <- atr_item(tabela)
+  
+  tabela <- tabela %>% 
+    filter(ITEM != "ITEM") %>%
+    filter(ocorrencia != "") %>% 
+    select(-ITEM) %>% 
+    gather(`capital`:`estado`, key = "local", value = "quantidade") %>% 
+    mutate(ano = ano,
+           tri =tri)
+}
 
+lista <- lst()
 
+for(ano in 1996:2016){
+  for(tri in 1:4){
+    tamanho <- length(lista)
+    if(ano == 2001){
+      lista[[tamanho + 1]]<- caso_2001(tri)
+    } else {
+      lista[[tamanho + 1]] <- caso_normal(ano, tri)
+    }
+  }
+}
+
+serie_df <- bind_rows(lista)
+
+serie_df <- serie_df %>% 
+  filter(!(ocorrencia %in% c("(EM FLAGRANTE+PREVENTIVA+POR MANDADO)", "(EM FLAGRANTE+POR MANDADO)", "(EM\r\n            FLAGRANTE+POR MANDADO)",
+                             ("EM FLAGRANTE+PREVENTIVA+POR MANDADO"))))
+
+serie_df <- serie_df %>% 
+  mutate(quantidade = parse_number(quantidade, locale = locale(decimal_mark = ",", grouping_mark = ".")),
+         quantidade = ifelse(is.na(quantidade), 0, quantidade)) %>% 
+  filter(!is.na(ano))
