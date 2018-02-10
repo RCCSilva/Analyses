95c44d599147da3c8bd55bd11a286288d47625ba
RCCSilva <rafa_castrosilva@hotmail.com>
2018-02-10 00:06:11 -0200

    alguns avanços no script e no WebScraping. Falta consolidar os dados mensais

--- [Trabalho]SSP/baixadados.R
+++ [Trabalho]SSP/baixadados.R
@@ -16,6 +16,12 @@ library(rvest)
 
 url_base <- "http://www.ssp.sp.gov.br/Estatistica/Pesquisa.aspx"
 
+mun_cod <- read_html(url_base) %>% 
+  html_nodes(xpath = "//select[@name = 'ctl00$conteudo$ddlMunicipios']/option")
+
+mun_cod <- tibble(cod      = as.numeric(html_attr(mun_cod, "value")),
+                  nome     = html_text(mun_cod))
+
 baixa_bo_municipio_ano  <- function(ano, municipio){
   
   pivot <- GET(url_base)
@@ -32,7 +38,42 @@ baixa_bo_municipio_ano  <- function(ano, municipio){
   
   params <- lst(
     `__EVENTARGUMENT`           = "",
-    `__EVENTTARGET`	            = "ctl00$conteudo$ddlMunicipios",
+    `__EVENTTARGET`	            = "ctl00$conteudo$btnMensal",
+    `__EVENTVALIDATION`         =	event_validation,
+    `__LASTFOCUS`               = "",
+    `__VIEWSTATE`	              = view_state,
+    `ctl00$conteudo$ddlAnos`	      = ano,
+    `ctl00$conteudo$ddlDelegacias`	= "0",
+    `ctl00$conteudo$ddlMunicipios`	= municipio,
+    `ctl00$conteudo$ddlRegioes`	    = "0"
+  )
+
+  tabela <- POST(url_base, body = params) %>% 
+    read_html() %>% 
+    html_table() %>% 
+    first() %>% 
+    mutate(municipio = municipio,
+           ano       = ano)
+}
+
+
+
+baixa_bo_dp_ano <- function(){
+  pivot <- GET(url_base)
+  
+  view_state <- pivot %>% 
+    read_html() %>% 
+    html_nodes("input[name='__VIEWSTATE']") %>% 
+    html_attr("value")
+  
+  event_validation <- pivot %>% 
+    read_html() %>% 
+    html_nodes("input[name='__EVENTVALIDATION']") %>% 
+    html_attr("value")
+  
+  params <- lst(
+    `__EVENTARGUMENT`           = "",
+    `__EVENTTARGET`	            = "ctl00$conteudo$ddlDelegacias",
     `__EVENTVALIDATION`         =	event_validation,
     `__LASTFOCUS`               = "",
     `__VIEWSTATE`	              = view_state,
@@ -51,29 +92,22 @@ baixa_bo_municipio_ano  <- function(ano, municipio){
            ano       = ano)
 }
 
-mun_cod <- read_html(url_base) %>% 
-  xml_find_all(xpath = "//select[@name = 'ctl00$conteudo$ddlMunicipios']/option")
-
-mun_cod <- tibble(cod = as.numeric(html_attr(mun_cod, "value")),
-                  nome= xml_text(mun_cod))
-
 # loop --------------------------------------------------------------------
 
-dados <- lst()
+serie_mensal_ls <- lst()
 
-for(ano in 2001:2017){
-  for(i in 1:655){
-    dados[[i]] <- baixa_bo_municipio_ano(str_c(ano), str_c(i))
-  }
-  
-  dados <- bind_rows(dados)
-  
-  dados %>% 
-    left_join(mun_cod, by = c("muni" = "cod")) %>% 
-    write_rds(str_c("dados",ano,".rds"))
+ano = 2017
+
+for(municipio in 1:645){
+    tamanho = length(serie_mensal_ls) + 1
+    
+    serie_mensal_ls[[tamanho]] <- baixa_bo_municipio_ano(ano, municipio)
+    
 }
 
+serie_mensa_df <- bind_rows(serie_mensal_ls)
 
+write_rds(serie_mensa_df, "[Trabalho]SSP/serie_mensal.rds")
 
 
 # atribuindo nome aos municipios ------------------------------------------
--- [Trabalho]SSP/script.R
+++ [Trabalho]SSP/script.R
@@ -4,6 +4,7 @@ library(ggthemes)
 library(tidyverse)
 library(sp)
 library(rgdal)
+library(readxl)
 
 saopaulo <- readOGR("[Base][SP]SaoPaulo", "35MUE250GC_SIR")
 
@@ -15,12 +16,47 @@ saopaulo_int <- saopaulo@data %>%
   arrange(NM_MUNICIP) %>% 
   mutate(id_alf = 1:645)
   
+pop_sao_paulo <- read_xlsx("[Trabalho]SSP/pop_esti_mun2001-2017.xlsx",
+                           skip = 3) %>% 
+  rename(mun = X__1) %>% 
+  select("mun", "2017") %>% 
+  mutate(lista = str_extract_all(mun, boundary("word")))
+
+pop_sao_paulo$uf <- NA
+pop_sao_paulo$cidade <- NA
+
+for(i in seq_along(pop_sao_paulo$lista)){
+  pop_sao_paulo$uf[i] <- pop_sao_paulo$lista[[i]][length(pop_sao_paulo$lista[[i]])]
+  
+  pop_sao_paulo$cidade[i] <- str_c(pop_sao_paulo$lista[[i]][-length(pop_sao_paulo$lista[[i]])], collapse = " ")
+}
+
+pop_sao_paulo <- pop_sao_paulo %>% 
+  filter(uf == "SP") %>% 
+  mutate(cidade = str_to_upper(cidade))
+
+saopaulo_int <- saopaulo_int %>% 
+  left_join(pop_sao_paulo, by = c("NM_MUNICIP" = "cidade"))
+
+serie_mensal_df <- read_rds("[Trabalho]SSP/serie_mensal.rds")
+
+serie_mensal_df <- serie_mensal_df %>% 
+  select(Natureza, Total, municipio, ano)
+
+total_mun_2017 <- serie_mensal_df %>% 
+  group_by(ano, municipio, Natureza) %>% 
+  summarise(soma = sum(quantidade)) %>% 
+  left_join(saopaulo_int, by = c("municipio" = "id_alf")) %>% 
+  mutate(taxa = (soma/`2017`) * 10000) %>% 
+  select(ano, municipio, uf, id, Natureza, taxa) %>% 
+  spread(key = "Natureza", value = "taxa")
 
 saopaulo_df <- saopaulo_df %>% 
-  left_join(saopaulo_int, by = "id")
+  left_join(total_mun_2017, by = "id")
 
 saopaulo_df %>% 
-  ggplot(mapping = aes(x = long, y = lat, group = group, fill = NM_MUNICIP == "SANTOS")) +
+  ggplot(mapping = aes(x = long, y = lat, group = group, fill = split(`HOMICÍDIO DOLOSO (2)`, f = c("baixo")))) +
   geom_polygon(color = "white") +
   coord_map() +
-  theme_map()
+  theme_map() +
+  scale_fill_discrete()
